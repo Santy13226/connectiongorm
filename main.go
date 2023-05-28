3f806def84f0e2ef9956db4d23b61c942e2ea16b
connectiongorm <santiguaylla@gmail.com>
2023-05-27 21:28:30 -0500

    CRUD con GORM

--- main.go
+++ main.go
@@ -20,12 +20,14 @@ func main() {
 	_ = conn
 	errorFatal(err)
 	//registrarCliente(conn)
-	consultarClientes(conn)
+	//consultarClientes(conn)
+	//consultarClientesPorCedula(conn, "0604855867")
 	//registrarSucursal(conn)
+	//updateCliente(conn)
+	deleteCliente(conn, "0604855866")
 	fmt.Println("Conexión exitosa")
 }
  //METODO DE REGISTRO
- /*
 func registrarCliente(conn *gorm.DB) {
 	cliente := &models.Cliente{
 		Cedula:             "0604855866",
@@ -42,10 +44,12 @@ func registrarCliente(conn *gorm.DB) {
 	result := conn.Create(cliente)
 	if result.Error != nil {
 		log.Println(result.Error)
+	}else{
+		log.Println("REGISTRO DE CLIENTE REALIZADO CORRECTAMENTE...")
 	}
 }
-*/
-/*
+
+
 func registrarSucursal(conn *gorm.DB) {
 	p := models.Sucursal{
 		Nombre:         "Farmacia",
@@ -56,17 +60,64 @@ func registrarSucursal(conn *gorm.DB) {
 	rs := conn.Create(p)
 	if rs.Error != nil {
 		log.Println(rs.Error)
+	}else{
+		log.Println("REGISTRO DE SUCURSAL REALIZADO CORRECTAMENTE...")
 	}
 }
-*/
-//METODO DE CONSULTA
+
+//METODO DE CONSULTA GENERAL
 func consultarClientes(conn *gorm.DB){
 	var clientes[]models.Cliente						//Se guarda el registro de clientes usando el modelo
 	if err:= conn.Find(&clientes).Error; err != nil{
 		log.Println(err.Error())						//manejo de error
+	}else{
+		log.Println("CONSULTA REALIZADA CORRECTAMENTE...")
 	}
 
 	for _, p := range clientes{ 			//Ciclo para imprimir los registros
 		fmt.Println(p)
+	}
+}
+
+//METODO DE CONSULTA POR PARÁMETRO
+func consultarClientesPorCedula(conn *gorm.DB, Cedula string){
+	var cliente models.Cliente						//Se guarda el registro de clientes usando el modelo
+	result := conn.Where("cedula=?", Cedula).Limit(1).Find(&cliente)
+	if result.Error != nil{
+		log.Println(result.Error)
+		return
+	}else{
+		log.Println("CONSULTA REALIZADA CORRECTAMENTE...")
+	}
+
+	if result.RowsAffected == 0{                    //Comprobacion si se encuentra el registro=1, si no se encuentra =0
+		log.Println("No se encontro el registro", Cedula)
+		return
+	}
+	fmt.Println(cliente)
+}
+
+func updateCliente(conn *gorm.DB){
+	var cliente=models.Cliente{Nombres: "Bryan Santiago", Apellidos: "Guaylla Ashqui", Edad: 22}
+	result := conn.Where("Cedula=?", "0604855866" ).Updates(&cliente)
+	if result.Error != nil{
+		log.Println(result.Error)
+		return
+	}else{
+		log.Println("REGISTRO ACTUALIZADO CORRECTAMENTE....")
+	}
+}
+
+func deleteCliente(conn *gorm.DB, Cedula string){
+	result := conn.Where("Cedula=?", Cedula).Delete(&models.Cliente{})
+	if result.Error != nil{
+		log.Println(result.Error)
+		return
+	}else{
+		log.Println("REGISTRO BORRADO CORRECTAMENTE....")
+	}
+	if result.RowsAffected == 0{                    //Comprobacion si se encuentra el registro=1, si no se encuentra =0
+		log.Println("NO SE ELIMINO", Cedula)
+		return
 	}
 }
